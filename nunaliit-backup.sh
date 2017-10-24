827e7af3825c32057386fa8bf1d3516cd3ab3a05
billingb <billingb@gmail.com>
2017-10-24 12:14:50 -0600

    Save daily, weekly and monthly backups with different deletion times.

--- nunaliit-backup.sh
+++ nunaliit-backup.sh
@@ -33,16 +33,39 @@ echo "Creating tarball"
 atlas_parent_dir=$(dirname ${atlas_dir})
 atlas_name=$(basename ${atlas_dir})
 
-tar_cmd="tar -czf ${backup_dir}/${atlas_name}_$(/bin/date +%Y%m%d_%H%M%S).tar.gz -C ${atlas_parent_dir} ${atlas_name}"
+month_day=`date +"%d"`
+week_day=`date +"%u"`
+
+if [ "$month_day" -eq 1 ] ; then #first day of month create monthly
+  backup_sub_dir=monthly
+else
+  if [ "$week_day" -eq 7 ] ; then #sunday create week
+    backup_sub_dir=weekly
+  else # On any regular day do
+    backup_sub_dir=daily
+  fi
+fi
+
+if [ ! -d "${backup_dir}" ]; then
+  echo "Backup directory does not exist: ${backup_dir}"
+  exit 1
+fi
+
+if [ ! -d "${backup_dir}/${backup_sub_dir}" ]; then
+  mkdir ${backup_dir}/${backup_sub_dir}
+fi
+
+tar_cmd="tar -czf ${backup_dir}/${backup_sub_dir}/${atlas_name}_$(/bin/date +%Y%m%d_%H%M%S).tar.gz -C ${atlas_parent_dir} ${atlas_name}"
 echo "Running tar command: ${tar_cmd}"
 ${tar_cmd}
 
-#delete all but newest 2 tar files
-#old_backups=$(ls -t ${backup_dir} | grep -v '/$' | tail -n +3)
-#for old_backup in ${old_backups}
-#do
-#  del_cmd="rm -r ${backup_dir}/${old_backup}"
-#  echo "Deleting old backup: ${del_cmd}"
-#  ${del_cmd}
-#done
+#delete all daily files older then 2 weeks
+find ${backup_dir}/daily/ -maxdepth 1 -mtime +14 -exec rm -rv {} \;
+
+#delete all weekly files older then 12 weeks
+find ${backup_dir}/weekly/ -maxdepth 1 -mtime +84 -exec rm -rv {} \;
+
+#delete all monthly files older then 2 years
+find ${backup_dir}/monthly/ -maxdepth 1 -mtime +730 -exec rm -rv {} \;
+
 cd -
